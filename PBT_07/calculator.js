017aa98b7ae11d4b1f90ca4808d8a32503190a55
Đỗ Hoàng Sơn <nndncm@gmail.com>
2026-05-22 19:48:13 +0700

    PBT_07: Hoàn thành B1

---
+++ PBT_07/calculator.js
@@ -0,0 +1,43 @@
+function calculate(num1, operator, num2) {
+
+    if (isNaN(num1) || isNaN(num2)) {
+        return "Lỗi: Input không phải số";
+    }
+
+    if (operator === "/" && num2 === 0) {
+        return "Lỗi: Không thể chia cho 0";
+    }
+
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+
+        case "-":
+            return num1 - num2;
+
+        case "*":
+            return num1 * num2;
+
+        case "/":
+            return num1 / num2;
+
+        case "%":
+            return num1 % num2;
+
+        case "**":
+            return num1 ** num2;
+
+        default:
+            return `Lỗi: Operator '${operator}' không hợp lệ`;
+    }
+}
+
+console.log(calculate(10, "+", 5));    
+
+console.log(calculate(10, "/", 0));    
+
+console.log(calculate(10, "^", 5));    
+
+console.log(calculate("abc", "+", 5)); 
+
+console.log(calculate(2, "**", 10));   
